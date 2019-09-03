62ee91c0aa1d46363b4d2c9881579eb47d9dbaa6
Catherine Etter <cetter.catherine.web@gmail.com>
2019-09-03 11:20:00 -0500

    Establish database connection

---
+++ Amarath/Data/AmarathContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Amarath.Models;
+
+/*
+ * Contains the Context for the Amarath Database, required for connection
+ */
+
+namespace Amarath.Data
+{
+    public class AmarathContext : DbContext
+    {
+        public AmarathContext(DbContextOptions<AmarathContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
--- Amarath/Startup.cs
+++ Amarath/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amarath.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace Amarath
 {
@@ -33,6 +35,9 @@ namespace Amarath
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<AmarathContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("AmarathDatabase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
