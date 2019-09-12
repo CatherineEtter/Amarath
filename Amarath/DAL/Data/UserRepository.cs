3aa4ca29a036b0fde420916f9d1901fd133bd504
Catherine Etter <cetter.catherine.web@gmail.com>
2019-09-12 13:56:42 -0500

    Setting up user repository

---
+++ Amarath/DAL/Data/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amarath.Models;
+
+namespace Amarath.DAL.Data
+{
+    /*public class UserRepository : IUserRepository
+    {
+        private AmarathContext context;
+    }*/
+}
