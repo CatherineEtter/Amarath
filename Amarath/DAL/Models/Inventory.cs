a94c96bab7d1d53eca7c7a5b565a37162e031500
Catherine Etter <cetter.catherine.web@gmail.com>
2019-11-06 13:14:51 -0600

    Finished Stats Modal and added Item/Inventory objects

---
+++ Amarath/DAL/Models/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amarath.Models
+{
+    [Table("Inventory")]
+    public class Inventory
+    {
+        [Key]
+        public int InvID { get; set; }
+        public int CharID { get; set; }
+        public int ItemID { get; set; }
+        public int Quantity { get; set; }
+        public Boolean Equiped { get; set; }
+    }
+}
---
+++ Amarath/DAL/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amarath.Models
+{
+    [Table("Item")]
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+        public int Damage { get; set; }
+        public int Defense { get; set; }
+    }
+}
