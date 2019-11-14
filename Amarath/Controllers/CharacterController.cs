66c93e840678f65692259d62db6484964da995e6
Hailey Martinez <hmartinez21@mail.stmarytx.edu>
2019-11-14 11:55:53 -0600

    Characters can now be added to the DB. Validation is only on character name, but there is no visual error response.

--- Amarath/Controllers/CharacterController.cs
+++ Amarath/Controllers/CharacterController.cs
@@ -5,11 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Amarath.DAL.Models;
+using Amarath.DAL.Data;
+using Amarath.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace Amarath.Controllers
 {
     public class CharacterController : Controller
     {
+        private readonly UserManager<IdentityUserExt> userManager;
+
+        public CharacterController(UserManager<IdentityUserExt> userManager)
+        {
+            this.userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -24,32 +34,30 @@ namespace Amarath.Controllers
         {
             if (ModelState.IsValid)
             {
-                //var character = new IdentityUserExt { Name = viewModel.Name, Rank = viewModel.Rank, Strength = viewModel.Strength, Intelligence = viewModel.Intelligence, Dexterity = viewModel.Dexterity, Health = viewModel.Health };
-                //var result = await characterManager.CreateAsync(user, viewModel.Password);
-                /*
-                if (result.Succeeded)
+                var optionsBuilder = new DbContextOptionsBuilder<AmarathContext>();
+                var db = new AmarathContext(optionsBuilder.Options);
+                var cUser = await userManager.GetUserAsync(User);
+
+                var character = new Character
                 {
-                    //TODO: Decide to use session or permanent cookie
-                    //await signInManager.SignInAsync(user, isPersistent: true);
-                    return RedirectToAction("Index", "Home");
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }*/
+                    UserId = cUser.Id,
+                    ClassTypeId = viewModel.ClassTypeId,
+                    Name = viewModel.Name,
+                    Rank = viewModel.Rank,
+                    Strength = viewModel.Strength,
+                    Intelligence = viewModel.Intelligence,
+                    Dexterity = viewModel.Dexterity,
+                    CurrentHealth = viewModel.CurrentHealth,
+                    MaxHealth = viewModel.CurrentHealth,
+                    Experience = 0,
+                    DungeonLevel = 0
+                };
+                db.Characters.Add(character);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
             }
 
-            return View(viewModel); //for errors
+            return View(viewModel);
         }
-        /*
-         * TODO: Instructions on how to make models/submission forms, and stuff
-         * DONE - Create a Character.cs model in DAL.Models (This MUST match what is in the SQL tables exactly. The same types, the keys, etc.
-         * DONE - Create a CharacterService.cs in DAL.Models (You can basically copy everything from CustomerService)
-         * DONE - Reference the Character model DBSet in AmarathContext.cs
-         * DONE - Create the CreateCharacterViewModel which should store all the form controls
-         *      - Create a method called CreateCharacter that takes in an instance of the CreateCharacterViewModel as a parameter (This is how forms are submitted, pls refer to the account setup)
-         *        - When a user submits a form by pressing the submit button, the request will go to the CreateCharacter.cshtml's CreateCharacterViewModel as an instance of the ViewModel.
-         *        - This method is where the code for creating a character should go since it will have direct access to all the form data
-         */
     }
 }
--- Amarath/DAL/Models/Character.cs
+++ Amarath/DAL/Models/Character.cs
@@ -13,7 +13,6 @@ namespace Amarath.Models
         [Key]
         public int CharId { get; set; }
         public string UserId { get; set; }
-        public int InventoryId { get; set; }
         public int ClassTypeId { get; set; }
         public string Name { get; set; }
         public int Rank { get; set; }
