a5a54a2712d97c439b80c2d6c9fb7324207893c8
Catherine Etter <cetter.catherine.web@gmail.com>
2019-11-17 17:15:18 -0600

    Created Codex

--- Amarath/Controllers/HomeController.cs
+++ Amarath/Controllers/HomeController.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Amarath.Models;
 using Amarath.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Amarath.DAL.Data;
 
 namespace Amarath.Controllers
 {
     public class HomeController : Controller
     {
+        private static DbContextOptionsBuilder<AmarathContext> optionsBuilder = new DbContextOptionsBuilder<AmarathContext>();
+        private static readonly AmarathContext db = new AmarathContext(optionsBuilder.Options);
+
         public IActionResult Index()
         {
             return View();
@@ -21,6 +26,22 @@ namespace Amarath.Controllers
             ViewData["Message"] = "Your application description page.";
 
             return View();
+        }
+        public async Task<IActionResult> Codex(string sortOrder)
+        {
+            ViewData["ItemData"] = String.IsNullOrEmpty(sortOrder) ? "name" : "";
+            var items = from i in db.Items select i;
+
+            switch(sortOrder)
+            {
+                case "name":
+                    items.OrderBy(i => i.Name);
+                    break;
+                default:
+                    items.OrderBy(i => i.Name);
+                    break;
+            }
+            return View(await items.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Privacy()
